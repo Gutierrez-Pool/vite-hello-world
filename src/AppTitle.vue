dbbcd1941f6d9acfd08587ee4e2620e4c50b3eb8
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-28 14:58:13 +0100

    first commit

---
+++ src/AppTitle.vue
@@ -0,0 +1,27 @@
+<script setup>
+import AppHero from './components/AppHero.vue'
+</script>
+
+<template>
+  <div class="jumbotron">
+    <div>
+      <a href="" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+    </div>
+    <AppHero msg="La mia prima app con Vite!" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
